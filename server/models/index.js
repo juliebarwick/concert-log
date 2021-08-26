285e0d969f39417a4fddeaa5593099f6d73bf7d8
Julie Barwick <julie@juliebarwick.com>
2021-08-26 11:09:48 -0700

    Formatting, and create getPhotos and updatePhoto model functions

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,29 @@
 const Entry = require('../db/models/Entry');
 
 module.exports = {
-  getEntries: () => Entry.find(),
+  getEntries: () => Entry.find().sort({ concertDate: -1 }),
 
-  createEntry: ({ title, entry, address }) => {
+  createEntry: ({
+    title,
+    entry,
+    address,
+    concertDate,
+  }) => {
     const newEntry = new Entry({
-      title, entry, address,
+      title, entry, address, concertDate,
     });
     return newEntry.save();
   },
+
+  getPhotos: (id) => Entry.findById(id).select('photos'),
+
+  updatePhoto: async (id, url) => {
+    // const photos = await Entry.findById(id);
+    // photos.push(url);
+    // return Entry.save();
+    return Entry.findByIdAndUpdate(id, {
+      $push: { photos: url },
+    });
+  },
+
 };

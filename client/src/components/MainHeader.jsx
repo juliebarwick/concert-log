d9b23481a75319a5ea5e926f0f5716ad5597fb80
Julie Barwick <julie@juliebarwick.com>
2021-08-26 11:07:47 -0700

    Create header component for site title

---
+++ client/src/components/MainHeader.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+  background: #b07b9a;
+  background: linear-gradient(345deg, #b07b9a, #65bbcc);
+  color: #fff;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 2.5em;
+  text-transform: uppercase;
+  letter-spacing: 0.1em;
+`;
+
+const MainHeader = () => (
+  <StyledHeader>
+    <Title>Concert Log</Title>
+    <h3>a place to log your concerts</h3>
+  </StyledHeader>
+);
+
+export default MainHeader;

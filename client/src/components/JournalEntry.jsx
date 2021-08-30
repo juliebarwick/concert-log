18ad57aa05e5ea471d88e32a8138ec3781d9e493
Julie Barwick <julie@juliebarwick.com>
2021-08-30 08:14:58 -0700

    Move functions to a helper file

--- client/src/components/JournalEntry.jsx
+++ client/src/components/JournalEntry.jsx
@@ -1,26 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { formatDate } from '../helpers';
-
-const removeFinalPunctuation = (word) => {
-  const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/;
-  if (punctuation.test(word[word.length - 1])) {
-    return word.split('').slice(0, word.length - 1).join('');
-  }
-  return word;
-};
-
-const truncate = (text, numOfWords = 5) => {
-  const splitText = text.split(' ');
-  if (splitText.length <= numOfWords) {
-    return text;
-  }
-  const finalWord = splitText[numOfWords - 1];
-  const newFinalWord = removeFinalPunctuation(finalWord);
-
-  const result = splitText.slice(0, numOfWords - 1).join(' ');
-  return `${result} ${newFinalWord}...`;
-};
+import { formatDate, removeFinalPunctuation, truncate } from '../helpers';
 
 const StyledEntry = styled.div`
   border-bottom: 1px solid #edebe6;
--- client/src/helpers/index.js
+++ client/src/helpers/index.js
@@ -7,3 +7,23 @@ export const formatDate = (date) => {
   ];
   return `${month} ${day}, ${year}`;
 };
+
+export const removeFinalPunctuation = (word) => {
+  const punctuation = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/;
+  if (punctuation.test(word[word.length - 1])) {
+    return word.split('').slice(0, word.length - 1).join('');
+  }
+  return word;
+};
+
+export const truncate = (text, numOfWords = 5) => {
+  const splitText = text.split(' ');
+  if (splitText.length <= numOfWords) {
+    return text;
+  }
+  const finalWord = splitText[numOfWords - 1];
+  const newFinalWord = removeFinalPunctuation(finalWord);
+
+  const result = splitText.slice(0, numOfWords - 1).join(' ');
+  return `${result} ${newFinalWord}...`;
+};

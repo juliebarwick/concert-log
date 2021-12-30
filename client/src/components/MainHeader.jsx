cf8bf741d5f9b6f01d1c865916cef581acef868c
Julie Barwick <julie@juliebarwick.com>
2021-12-29 20:56:58 -0800

    Styling and pass search functionality to Search component

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import Search from './Search';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import Search from './Search';
 
 const StyledHeader = styled.header`
   background: #b07b9a;
   background: linear-gradient(345deg, #b07b9a, #65bbcc);
   color: #fff;
   padding: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
 `;
 
 const Title = styled.h1`
@@ -15,12 +19,18 @@ const Title = styled.h1`
   letter-spacing: 0.1em;
 `;
 
-const MainHeader = () => (
+const MainHeader = ({ onSearch }) => (
   <StyledHeader>
-    <Title>Concert Log</Title>
-    <h3>a place to log your concerts</h3>
-    <Search />
+    <div>
+      <Title>Concert Log</Title>
+      <h3>a place to log your concerts</h3>
+    </div>
+    <Search onSearch={onSearch} />
   </StyledHeader>
 );
 
 export default MainHeader;
+
+MainHeader.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+};

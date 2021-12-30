e24db95006c40a91d478e42f6f4ddd7f5475f5b0
Julie Barwick <julie@juliebarwick.com>
2021-12-29 20:54:40 -0800

    Change EntryMap component according to updated docs

--- client/src/components/EntryMap.jsx
+++ client/src/components/EntryMap.jsx
@@ -1,42 +1,33 @@
 import React from 'react';
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import PropTypes from 'prop-types';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import GOOGLE_API_KEY from '../../config';
 
 const containerStyle = {
   width: '100%',
   height: '300px',
+  marginTop: '30px',
 };
 
-function EntryMap({ location }) {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: GOOGLE_API_KEY,
-  });
-
-  const [map, setMap] = React.useState(null);
-
-  // TODO - Fix, Makes zoom broken
-  // const onLoad = React.useCallback(function callback(map) {
-  //   const bounds = new window.google.maps.LatLngBounds();
-  //   map.fitBounds(bounds);
-  //   setMap(map)
-  // }, []);
-
-  const onUnmount = React.useCallback((map) => {
-    setMap(null);
-  }, []);
-
-  return isLoaded ? (
+const EntryMap = ({ location }) => (
+  <LoadScript
+    googleMapsApiKey={GOOGLE_API_KEY}
+  >
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={location}
-      // onLoad={onLoad}
       zoom={15}
-      onUnmount={onUnmount}
     >
       <Marker position={location} />
     </GoogleMap>
-  ) : <></>;
-}
+  </LoadScript>
+);
 
 export default React.memo(EntryMap);
+
+EntryMap.propTypes = {
+  location: PropTypes.shape({
+    lat: PropTypes.number,
+    lng: PropTypes.number,
+  }).isRequired,
+};

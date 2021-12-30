42a0e56d04235a49a9064b0732a1534415778643
Julie Barwick <julie@juliebarwick.com>
2021-12-29 20:55:23 -0800

    Move styling from CSS to styled-components, conditional rendering for uploaded status, proptypes validation

--- client/src/components/UploadPhoto.jsx
+++ client/src/components/UploadPhoto.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { MdAddAPhoto } from 'react-icons/md';
 import styled from 'styled-components';
 
-const UploadPhoto = ({ doPatch, handleFileInputChange }) => (
-  <StyledForm>
-    <label htmlFor="file" className="custom-label">
-      <MdAddAPhoto />
-      &nbsp;&nbsp;Upload Your photos
-      <input type="file" id="file" className="custom-file-input" onChange={handleFileInputChange} />
-    </label>
-    <ButtonDiv>
-      <button type="button" onClick={doPatch}>Submit</button>
-    </ButtonDiv>
+const UploadPhoto = ({ onSubmit, handleFileInputChange, hasBeenUploaded }) => (
+  <StyledForm onSubmit={onSubmit}>
+    {hasBeenUploaded ? (
+      <ButtonDiv>
+        <button type="submit">Submit Photo</button>
+      </ButtonDiv>
+    ) : (
+      <Label htmlFor="file" className="custom-label">
+        <MdAddAPhoto />
+        &nbsp;&nbsp;Upload a photo
+        <input type="file" id="file" className="custom-file-input" onChange={handleFileInputChange} />
+      </Label>
+    )}
   </StyledForm>
 );
 
@@ -26,4 +30,28 @@ const StyledForm = styled.form`
   align-items: center;
 `;
 
+const Label = styled.label`
+  padding: 10px 20px;
+  font-family: 'Roboto Slab', serif;
+  font-size: .8em;
+  text-transform: uppercase;
+  border: 1px solid #272727;
+  display: block;
+  width: auto;
+  cursor: pointer;
+  background-color: #272727;
+  color: #fff;
+  text-align: center;
+  border-radius: 20px;
+  &:hover {
+    color: #a5a5a5;
+  }
+`;
+
 export default UploadPhoto;
+
+UploadPhoto.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+  handleFileInputChange: PropTypes.func.isRequired,
+  hasBeenUploaded: PropTypes.bool.isRequired,
+};
